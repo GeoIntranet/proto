22ef03ef2b87bab5347b8283a1d1e49e2330d170
GeoIntranet <valero.geoffrey06@gmail.com>
2017-07-11 23:41:30 +0200

    init proto

---
+++ resources/views/euro/home.blade.php
@@ -0,0 +1,266 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Eurocomputer</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+</head>
+<body>
+
+{{--<div class="container-fluid">--}}
+    {{--<div class="row  visible-xs">--}}
+        {{--<div class="item visible-lg col-lg-1  ">  </div>--}}
+        {{--<div class="col ">visible XSsmall</div>--}}
+        {{--<div class="item visible-lg col-lg-1  ">  </div>--}}
+    {{--</div>--}}
+    {{--<div class="row  visible-sm">--}}
+        {{--<div class="item visible-lg col-lg-1  ">  </div>--}}
+        {{--<div class="col ">visible Small</div>--}}
+        {{--<div class="item visible-lg col-lg-1  ">  </div>--}}
+    {{--</div>--}}
+    {{--<div class="row  visible-md">--}}
+        {{--<div class="item visible-lg col-lg-1  ">  </div>--}}
+        {{--<div class="col ">visible Medium</div>--}}
+        {{--<div class="item visible-lg col-lg-1  ">  </div>--}}
+    {{--</div>--}}
+    {{--<div class="row  visible-lg">--}}
+        {{--<div class="item visible-lg col-lg-1  ">  </div>--}}
+        {{--<div class="col ">visible Large</div>--}}
+        {{--<div class="item visible-lg col-lg-1  ">  </div>--}}
+    {{--</div>--}}
+{{--</div>--}}
+
+{{-- BAR CONTACT-------------}}
+<div class="container-fluid">
+    <div class="row contact">
+        <div class="item visible-lg col-lg-1  ">  </div>
+        <div class=" col-lg-10 col-md-12  col-sm-12  text-right"><h5>CONTACT / TEL / AUTRE</h5></div>
+        <div class="item visible-lg col-lg-1  ">  </div>
+    </div>
+</div>
+{{-- FIN BAR CONTACT-------------}}
+
+{{-- LOGO / MENU-------------}}
+<div class="container-fluid menu">
+    <div class="row row-eq-height ">
+        <div class="item visible-lg col-lg-1  ">  </div>
+        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 logo "><h1>LOGO</h1></div>
+
+        <div class="col-lg-7 col-md-9  col-sm-9 col-xs-6   text-right">
+            <div class="row menu-item text-center">
+                <div class="item  visible-sm visible-md visible-lg  col-sm-2 col-md-2 col-lg-2">SOCIETE</div>
+                <div class="item  visible-sm visible-md visible-lg  col-sm-2 col-md-2 col-lg-2">TRACABILITE</div>
+                <div class="item  visible-sm visible-md visible-lg  col-sm-2 col-md-2 col-lg-2">IMPRIMANTES</div>
+                <div class="item  visible-sm visible-md visible-lg  col-sm-2 col-md-2 col-lg-1">MICRO</div>
+                <div class="item  visible-sm visible-md visible-lg  col-sm-2 col-md-2 col-lg-2">REPARATION & SERVICES</div>
+                <div class="item  visible-sm visible-md visible-lg  col-sm-2 col-md-2 col-lg-2">MARQUES</div>
+                <div class="item visible-lg   col-lg-1">XX</div>
+            </div>
+
+        </div>
+        <div class="item visible-lg col-lg-1  ">  </div>
+    </div>
+</div>
+{{-- FIN LOGO / MENU-------------}}
+
+{{-- BANNIERE-------------}}
+<div class="container-fluid banniere hidden">
+    <div class="row ">
+        <div class="col-lg-12 col-md-12  col-sm-12"><h1>BANNIERE</h1></div>
+    </div>
+</div>
+{{-- FIN BANNIERE-------------}}
+
+{{-- BANNIERE-------------}}
+<div class="container-fluid ">
+    <div class="row">
+        <div class="visible-lg col-lg-4 col-md-12  col-sm-12 video"><h1>VIDEO </h1></div>
+        <div class="col-lg-8 col-md-12  col-sm-12 banniere"><h1>BANNIERE</h1></div>
+    </div>
+</div>
+{{-- FIN BANNIERE-------------}}
+
+{{-- SOLUTIONS  / PRODUITS-------------}}
+<div class="container-fluid">
+    <div class="row row-eq-height">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+            <div class="row prestation">
+                <div class="item visible-lg col-lg-1  ">  </div>
+                <div class="item col-xs-12 col-sm-12 col-md-12 col-lg-10  text-center">
+                    <span style="font-size: 2.5em;"> <b>NOS</b> <b style="color: #3097D1">SOLUTIONS</b> </span>
+                </div>
+                <div class="item visible-lg col-lg-1  ">  </div>
+            </div>
+
+            <div class="row solution  text-center">
+                <div class="item visible-lg col-lg-1  "></div>
+
+                <div class="wraper col-xs-6 col-sm-4 col-md-2 col-lg-2 ">
+                    <div class="item"><h3>TRACABILITE</h3></div>
+                </div>
+                <div class="wraper col-xs-6 col-sm-4 col-md-3 col-lg-2  ">
+                    <div class="item"><h3>TRACABILITE</h3></div>
+                </div>
+                <div class="wraper col-xs-6 col-sm-4 col-md-2 col-lg-2  ">
+                    <div class="item"><h3>TRACABILITE</h3></div>
+                </div>
+                <div class="wraper col-xs-6 col-sm-6 col-md-3 col-lg-2  ">
+                    <div class="item"><h3>TRACABILITE</h3></div>
+                </div>
+                <div class="wraper col-xs-12 col-sm-6 col-md-2 col-lg-2  ">
+                    <div class="item"><h3>TRACABILITE</h3></div>
+                </div>
+
+                <div class="item visible-lg col-lg-1  "></div>
+            </div>
+
+        </div>
+    </div>
+    {{-- FIN SOLUTIONS  / PRODUITS-------------}}
+</div>
+<br>
+{{-- CONTENUE-------------}}
+<div class="container-fluid content">
+    <div class="row ">
+        <div class="col-sm-12 col-lg-12 col-md-12 ">
+            <div class="row">
+                <div class="item visible-lg col-lg-1  ">  </div>
+                <div class="col-sm-12 col-md-12  col-lg-7 ">
+                    <div class="row">
+                        <div class="col">
+                           <h1> DECOUVRIR EUROCOMPUTER EN VIDEO</h1>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-12 col-md-12">
+
+                            <p>
+                                Vente de matériels de traçabilité reconditionnés
+                                (lecteur, scanner code barre, imprimante Zebra, etc.) et réparation.
+                            </p>
+                            <p>
+                                Depuis 25 ans, Eurocomputer propose son savoir-faire en matière d’optimisation des coûts informatiques.
+
+                                Consultez nos offres de déstockage en traçabilité : imprimantes étiquettes & lecteurs code-barres
+                            </p>
+
+                            <ul>
+                                <li>
+                                    toutes les plus grandes marques sont en stock : Zebra Motorola Symbol – Datalogic – Psion Teklogix – Intermec
+                                </li>
+                            </ul>
+
+                            <p>
+                                Profitez de nos promotions sur les
+                                périphériques de bureau : PC – écrans – portables – platines clients-légers - imprimantes laser – imprimantes matricielles
+                            </p>
+
+                            <ul>
+                                <li>
+                                    marques HP – Lenovo – Dell Wyse – Lexmark – Epson – Oki
+                                </li>
+                            </ul>
+
+                            <p>
+                                Vous recherchez d’anciennes références, du matériel informatique reconditionné ou en déstockage,
+                                des accessoires & pièces détachées : contactez-nous, une réponse vous sera apportée dans les 24 heures.
+                            </p>
+
+                            <p>
+                                Pour tous ces matériels commercialisés, des prestations d’échange, de reconditionnement, de réparation, de location sont proposés.
+                            </p>
+                            <p>
+                                Envoyez-nous vos périphériques en panne : terminaux mobiles, scanners code-barres et imprimantes
+                                thermiques etc… Un devis gratuit de réparation vous sera adressé immédiatement. La réparation est
+                                effectuée dans nos locaux, au siège français du groupe. Le matériel réparé vous sera retourné dans des délais reconnus comme
+                                étant les plus courts du marché. Eurocomputer est partout plébiscité pour sa réactivité, y compris hors contrat de maintenance.
+                            </p>
+
+                            <p>
+                                La reprise et la valorisation
+                                de vos imprimantes d’occasion en panne est possible, dans le cadre de remplacement par du matériel reconditionné ou neuf.
+                            </p>
+                            <p>
+                                Tous nos matériels, neufs ou reconditionnés sont garantis jusqu’à 3 ou 5 ans
+                            </p>
+                            <p>
+                                Eurocomputer : 25 ans d’expertise auprès d’enseignes de l’agroalimentaire, du transport et de la logistique, de l’industrie.
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-sm-12 col-md-12  col-lg-4">
+                    <div class="row text-center">
+                        <div class="col-md-12"><h1>PHOTOS</h1></div>
+                    </div>
+                    <div class="row text-center" style="padding-bottom: 3px">
+                        <div class="col-lg-6 col-md-6 photo_w " style="padding: 3px">
+                            <div class=""
+                                 style="
+                                 background-color: #8eb4cb;
+                                 min-height: 100px;
+                                 color: white;
+                                 font-weight: bold;
+                                 padding-top: 20px "
+
+                            >1</div>
+                        </div>
+
+                        <div class="col-lg-6 col-md-6 photo_w " style="padding: 3px">
+                            <div class=""
+                                 style="
+                                 background-color: #8eb4cb;
+                                 min-height: 100px;
+                                 color: white;
+                                 font-weight: bold;
+                                 padding-top: 20px "
+
+                            >1</div>
+                        </div>
+                    </div>
+
+                    <div class="row text-center" style="padding-bottom: 3px">
+                        <div class="col-lg-6 col-md-6 photo_w " style="padding: 3px">
+                            <div class=""
+                                 style="
+                                 background-color: #8eb4cb;
+                                 min-height: 100px;
+                                 color: white;
+                                 font-weight: bold;
+                                 padding-top: 20px "
+
+                            >1</div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 photo_w " style="padding: 3px">
+                            <div class=""
+                                 style="
+                                 background-color: #8eb4cb;
+                                 min-height: 100px;
+                                 color: white;
+                                 font-weight: bold;
+                                 padding-top: 20px "
+
+                            >1</div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="item hidden-sm hidden-md col-xs-1 col-sm-1 col-md-1 col-lg-1 text-center "></div>
+            </div>
+            <br>
+        </div>
+    </div>
+</div>
+{{-- CONTENUE-------------}}
+
+
+<br><br><br><br>
+
+</body>
+
+</html>
