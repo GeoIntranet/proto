709527f5aa25f1e5bdd84f85ccc48a8c73cdf0eb
valero.geoffrey06@gmail.com <valero.geoffrey06@gmail.com>
2017-07-17 17:44:31 +0200

    test

--- resources/views/euro/component/baniere/baniere.blade.php
+++ resources/views/euro/component/baniere/baniere.blade.php
@@ -1,7 +1,17 @@
 <div class="container-fluid ">
     <div class="row ">
-        <div class="col wrapper-baniere">
-            <img src="imgs/info.jpg" alt="informatique">
+        {{--<div class="col wrapper-baniere">--}}
+            {{--<img src="imgs/_ban.jpg" alt="informatique">--}}
+        {{--</div>--}}
+        <div class="container-imgs" style="
+        background-position: center;
+    background-repeat: no-repeat;
+     background-size: cover;
+        height: 400px; width:  100%; background-image:url(imgs/_ban.jpg)">
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
         </div>
     </div>
 </div>
